29f238a4d969cdcac494e39260e1029ccec45198
Ashish Magnani <ashishmagnani07@gmail.com>
2026-03-01 20:24:13 +0530

    removed comments

--- src/components/TrustSection.jsx
+++ src/components/TrustSection.jsx
@@ -21,10 +21,7 @@ const TrustSection = () => {
 
         {/* LEFT STORY */}
         <div>
-          {/* <h2 className="text-sm tracking-widest text-gray-500 mb-3">
-            ACT 5
-          </h2> */}
-
+          
           <h3 className="text-4xl font-bold text-gray-900 mb-6 leading-tight">
             Why You Can <span className="text-indigo-600">Trust Me</span>
           </h3>
